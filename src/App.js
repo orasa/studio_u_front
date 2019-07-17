2a7ce8f5bc726f06bdb3ebe0cf349b80c7f7fb97
orasa oneal <orasaoneal@gmail.com>
2019-07-16 22:00:27 -0700

    first commit with git ignore update

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+class App extends Component {
+
+
+
+
+//Render
+render() {
+  return(
+
+
+			<h1>Hellow World</h1>
+
+  )
+ }
+}
+
+export default App;
