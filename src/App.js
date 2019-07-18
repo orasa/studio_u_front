3813add3705b7784e9cb1a7e9a2947f3e0b5bc49
orasa oneal <orasaoneal@gmail.com>
2019-07-17 22:35:11 -0700

    styling home page with SignUp and Log in button

--- src/App.js
+++ src/App.js
@@ -11,34 +11,35 @@ class App extends Component {
 	//Render
 	render() {
 		return(
-			<div className="wrap bg-light">
-				<div className="nav un-styled">
-					<div className="ul li-unstyled">
-						<li >SignUp</li>
-						<li>LogIn</li>
-					</div>
+			<div className="wrap ">
+				<div id="title" className="row">
+					<p className="title text-left">Studio Unicorns</p>
+					<nav className="button text-right ">
+						<a className="btn btn-info mr-2" href="#" role="button">SignUp</a>
+						<a className="btn btn-info mr-2" href="#" role="button">Log In</a>
+					</nav>
 				</div>
+
+
 				<div id="header" className="row bg-light">
 
-					<h1 className="title">Studio Unicorns</h1>
-
-					<div id="main" className="row bg-secondary p-p1 ">
-  					<div id="sidebar" className="col-2 bg-light">
-  						<h5>Categories</h5>
-  						<ul>
+					<div id="main" className="row p-p1 ">
+  					<div id="sidebar" className="col-2 ">
+  						<h4>Categories</h4>
+  						<div className="ul ">
   							<li>Web Application</li>
   							<li>JavaScripts</li>
   							<li>Python</li>
   							<li>Git</li>
-  						</ul>
+  						</div>
   					</div>
-  					<div id="videoContent" className="col-9 bg-warning">
-  						<div className="d-flex flex-row mb-2 bg-primary p-3">
+  					<div id="videoContent" className="col-9 ">
+  						<div className="d-flex flex-row mb-2 p-3">
   							<div className="col bg-light mr-2 p-5">Flex item 1</div>
   							<div className="col bg-light mr-2 p-5">Flex item 2</div>
   							<div className="col bg-light p-5">Flex item 3</div>
   						</div>
-  						<div className="d-flex flex-row mb-3 bg-primary p-2">
+  						<div className="d-flex flex-row mb-3 p-3">
   							<div className="col p-2 bg-light mr-2 p-5">Flex item 4</div>
   							<div className="col p-2 bg-light mr-2 p-5">Flex item 5</div>
   							<div className="col p-2 bg-light p-5">Flex item 6</div>
