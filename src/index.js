1caa3feec0a1c118f5b94fb942ba41c08c11f2eb
orasa oneal <orasaoneal@gmail.com>
2019-07-30 21:44:01 -0700

    update route

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Routes from './Routes'
 import * as serviceWorker from './serviceWorker';
-import Test3 from './Test3';
-// import App from './App';
+import App from './App';
 
-ReactDOM.render(<Test3 />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
