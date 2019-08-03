8f0a1c7cd7fe0b48d0be8e4beaf6e182a0bb50cb
orasa oneal <orasaoneal@gmail.com>
2019-08-03 00:44:44 -0700

    add get_videosByUser, update query in models/video

--- src/profile.js
+++ src/profile.js
@@ -8,13 +8,13 @@ import Video from './Video'
 class Profile extends Component {
 	// Data
 	state = {
-		user: '',
+		user: {},
 		email: '',
-		password: ''
+		password: '',
 
-		// likes: [],
-		// fans: [],
-		// videos: []
+		likes: [],
+		fans: [],
+		videos: []
 	}
 	//Functions
 	get_loggedUser = () => {
@@ -27,14 +27,34 @@ class Profile extends Component {
 	        console.log("****** user from App.js", res.data)
 	        this.setState({
 						user: res.data
+					}, () => {
+						this.get_videosByUser();
+						console.log("videos", this.state.videos)
 
 					})
 	      }).catch((err) => { "Nooo, bloody way!"
 		})
 	}
+
+	get_videosByUser = () => {
+		axios.get(`http://localhost:4000/api/videos?user=${this.state.user._id}`,
+			{	headers: {
+					Authorization: `Bearer ${localStorage.getItem("token")}`
+				}
+			})
+			.then(res => {
+				console.log("****** Video from get_videosByUser", res.data)
+				console.log(`http://localhost:4000/api/profile?user=${this.state.user._id}`)
+				this.setState({
+					videos: res.data
+				}, () => console.log('videos after setState', this.state.videos))
+			}).catch((err) => { console.log(">>>>> Err from get Video by Users Nooo, bloody way!",err)
+	})
+}
+
+	//need to call compomentWillMount here to load life cycle of //profile as profile is a component by itself
 	componentWillMount() {
 		this.get_loggedUser()
-
 	}
 
 
@@ -44,24 +64,55 @@ class Profile extends Component {
 
 			<div className="profileWrap">
 				<h2 className="profileTitle ">Your Profile</h2>
-				<div className="userDetails">
+				<div className="userInfo">
+				 <div className="userDetails">
+					 <table>
+						 <tbody>
+							 <tr >
+								<td className="info">UserName:</td>
+								<td className="userName"> {this.state.user.name}</td>
+								</tr>
+								{/* email */}
+								<tr >
+ 								<td className="info">Email:</td>
+ 								<td className="userName"> {this.state.user.email}</td>
+ 								</tr>
+								{/**/}
+								<tr >
+ 								<td className="info">Your List(Videos Posted by you):</td>
+ 								<td className="userName"> {this.state.user.videos}</td>
+ 								</tr>
+								{/*Your Favorite list of vedeos that you likes*/}
+								{/*Your likes sum of all likes you get from others*/}
+								<tr >
+ 								<td className="info">Fans:</td>
+								{/*
+									<td class="userName"> {this.state.video.fans}</td>*/}
+
+ 						</tr>
+								{/**/}
+						 </tbody>
+					</table>
+				</div>
+				<div className="videoList">
 					<table>
 						 <tbody>
 							 <tr >
-								<td class="userName">Your User's Name: {this.state.user.name}</td>
+								<td className="yourList">Your video Lists(posted by you): {this.state.user.name}</td>
 								</tr>
 								<tr>
-									<td class="userEmail">Your Email: {this.state.user.email}</td>
+									<td className="likesByOhers">Your likes: {this.state.user.email}</td>
 								</tr>
 								<tr>
 									{/* <td>Your fans: {this.state.video.fans}</td>*/}
-
 								</tr>
-
 						 </tbody>
 					</table>
-
 				</div>
+			</div>
+				{/* videoListVideos posted by this user*/}
+
+				{/* end videoList*/}
 			</div>
 		);
 	}
