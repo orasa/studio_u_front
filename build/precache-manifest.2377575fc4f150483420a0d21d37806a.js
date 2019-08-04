25b229d8288ace820a1b5def33da83763ae2d6ac
orasa oneal <orasaoneal@gmail.com>
2019-08-04 01:33:51 -0700

    optimized production build constructed

---
+++ build/precache-manifest.2377575fc4f150483420a0d21d37806a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a79100c4a66f82beed93900923e493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf51ce8d78f7f82d944e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9267f945ce0b9ef7bf25",
+    "url": "/static/css/main.95c2056e.chunk.css"
+  },
+  {
+    "revision": "cf51ce8d78f7f82d944e",
+    "url": "/static/js/2.db7aa8f8.chunk.js"
+  },
+  {
+    "revision": "9267f945ce0b9ef7bf25",
+    "url": "/static/js/main.f76a7374.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
